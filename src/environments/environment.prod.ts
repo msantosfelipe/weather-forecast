ed05fe1c21365c99aa656a8be6530a8982178e1c
Felipe Maia <felipe_356@hotmail.com>
2018-03-24 11:21:47 -0300

    https

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,5 +1,5 @@
 export const environment = {
   production: true,
   apiKey: '20fd741e131d55a36c2e265ab81d039e',
-  apiRoot: 'http://api.openweathermap.org/data/2.5'
+  apiRoot: 'https://api.openweathermap.org/data/2.5'
 };
