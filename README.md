46057f6b39f5d7b0eb99f070d8ac7001f9bb6984
Felipe Maia Santos <felipe.santos@SLT-002078.solutis.net.br>
2018-03-26 13:20:23 -0300

    Ajustes resposividade

--- README.md
+++ README.md
@@ -1,7 +1,5 @@
 # WeatherForecast
-
 Deploy no Heroku: https://fathomless-chamber-48795.herokuapp.com
-
 
 Weather Forecast
 Angular 5 application on development.
@@ -10,7 +8,7 @@ Branch master -> data saved on local storage
 This project was generated with [Angular CLI](https://github.com/angular/angular-cli) version 1.7.3.
 
 ## Production server
-Run 'npm install', 'npm start' for a prod server. Navigate to 'http://localhost:8080'.
+Run 'npm install' and 'npm start' for a prod server. Navigate to 'http://localhost:8080'.
 
 ## Development server
 Run `ng serve` for a dev server. Navigate to `http://localhost:4200/`. The app will automatically reload if you change any of the source files.
@@ -20,6 +18,7 @@ Run `ng build` to build the project. The build artifacts will be stored in the `
 
 ## Running unit tests
 Run `ng test` to execute the unit tests via [Karma](https://karma-runner.github.io).
+Run `ng test --code-coverage=true` to execute the unit tests and generate a log with the code coverage percentage. It will generate the 'coverage/' directory. Then open coverage/index.html.
 
 ## Running end-to-end tests
 Run `ng e2e` to execute the end-to-end tests via [Protractor](http://www.protractortest.org/).
--- src/app/servico/weather.service.spec.ts
+++ src/app/servico/weather.service.spec.ts
@@ -1,13 +1,15 @@
-import { HttpClient, HttpClientModule } from '@angular/common/http';
+import { environment } from './../../environments/environment';
+import { HttpClient, HttpClientModule, HttpParams } from '@angular/common/http';
 import { TestBed, inject } from '@angular/core/testing';
 
 import { WeatherService } from './weather.service';
+import { HttpClientTestingModule, HttpTestingController } from '@angular/common/http/testing';
 
 describe('WeatherService', () => {
   beforeEach(() => {
     TestBed.configureTestingModule({
       imports: [
-        HttpClientModule
+        HttpClientTestingModule
       ],
       providers: [
         WeatherService,
@@ -19,4 +21,22 @@ describe('WeatherService', () => {
   it('should be created', inject([WeatherService], (service: WeatherService) => {
     expect(service).toBeTruthy();
   }));
+
+  it('should get profile data of user', () => {
+    const profileInfo = { };
+    const weatherService = TestBed.get(WeatherService);
+    const http = TestBed.get(HttpTestingController);
+    let profileResponse;
+
+    weatherService.buscarCidadesPorNome('Salvador').subscribe((response) => {
+      profileResponse = response;
+    });
+    const param = new HttpParams()
+    .append('q', 'Salvador')
+    .append('lang', 'pt')
+    .append('appid', environment.apiKey);
+    http.expectOne('https://api.openweathermap.org/data/2.5//find', {params: param}).flush(profileInfo);
+    expect(profileResponse).toEqual(profileInfo);
+  });
+
 });
